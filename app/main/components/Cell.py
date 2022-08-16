e518b238035dfd471dce7b08c0e216594ec680e2
Marco Luzzara <marco.luzzara@hotmail.it>
2022-08-16 11:33:31 +0200

    complete cell multiple selection

--- app/main/components/Cell.py
+++ app/main/components/Cell.py
@@ -1,9 +1,7 @@
 from PySide6 import QtCore, QtWidgets, QtGui
 
-from ..App import App
-
 class Cell(QtWidgets.QLabel):
-    def __init__(self, parent: App, text: str):
+    def __init__(self, parent, text: str):
         super().__init__()
 
         self.parent = parent
@@ -11,11 +9,16 @@ class Cell(QtWidgets.QLabel):
         self.setText(text)
         self.selected = False
 
-    def mousePressEvent(self, ev: QtGui.QMouseEvent) -> None:
+    def is_point_inside(self, point: QtCore.QPoint) -> bool:
+        return (self.x() <= point.x() <= self.x() + self.width()) and \
+            (self.y() <= point.y() <= self.y() + self.height())
+
+    def toggle_selection(self):
         self.selected = not self.selected
         if self.selected:
-            self.setStyleSheet('background-color: #9BF3FF') # default
+            self.setStyleSheet('background-color: #9BF3FF') # light blue
         else:
-            self.setStyleSheet('background-color: #F5F5F5') # light blue
+            self.setStyleSheet('background-color: #F5F5F5') # default
 
-        self.parent.cell_pressed.emit()
+    def mouseReleaseEvent(self, ev: QtGui.QMouseEvent) -> None:
+        self.parent.selection_end.emit(self, ev.scenePosition())
